f7ee3e53550e01fda3e28c927fc38a9033aa3c0f
‘AMAYA <aamaya547@gmail.com>
2022-06-27 13:02:10 -0700

    added sign up function

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -3,13 +3,44 @@ import { signUp } from './services/fetch-utils';
 
 export default function AuthPage({ setUser }) {
   const [error, setError] = useState('');
-  const [sighnUpEmail, setSignUpEmail] = useState('');
-  const [sighnUpPassword, setSignUpPassword] = useState('');
+  const [signUpEmail, setSignUpEmail] = useState('');
+  const [signUpPassword, setSignUpPassword] = useState('');
+
+  async function handleSignUpSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const user = await signUp(signUpEmail, signUpPassword);
+      setUser(user);
+    } catch (e) {
+      setError(e.message);
+    }
+  }
 
   return (
     <div>
       <h3>Secret is Sacred</h3>
-      <h1>{error}</h1>
+      <h1 className="error">{error}</h1>
+      <form>
+        <p>Sign Up!</p>
+        <label>
+          email
+          <input
+            onChange={(e) => setSignUpEmail(e.target.value)}
+            value={signUpEmail}
+            type="email"
+          />
+        </label>
+        <label>
+          password
+          <input
+            onChange={(e) => setSignUpPassword(e.target.value)}
+            value={signUpPassword}
+            type="password"
+          />
+        </label>
+        <button>Sign Up</button>
+      </form>
     </div>
   );
 }
