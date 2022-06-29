ffc0a18300888a3bb65e20e1ff5874f9c50dd9ca
‘AMAYA <aamaya547@gmail.com>
2022-06-28 17:53:02 -0700

    hardcoded update form

--- src/UpdatePage.js
+++ src/UpdatePage.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import { getTreasureById, updateTreasure } from './services/fetch-utils';
 
 export default function UpdatePage() {
+  const { push } = useHistory();
+  const { id } = useParams();
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [source, setSource] = useState('');
+  const [rating, setRating] = useState('');
+
+  useEffect(() => {
+    async function doFetch() {
+      const treasure = await getTreasureById(id);
+
+      setName(treasure.name);
+      setDescription(treasure.description);
+      setSource(treasure.source);
+      setRating(treasure.rating);
+    }
+
+    doFetch();
+  }, [id]);
+
+
   return (
-    <div>UpdatePage</div>
+    <div>
+      <h3>Update Your Treasure Specs</h3>
+      <form>
+        <label>
+          Name:
+          <input />
+        </label>
+        <label>
+          Description:
+          <input />
+        </label>
+        <label>
+          Source:
+          <input />
+        </label>
+        <label>
+          Rating:
+          <input type="number" />
+        </label>
+      </form>
+    </div>
   );
 }
