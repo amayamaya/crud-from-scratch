a05f9a1c60c13db030c14009866bf963c284a5fe
‘AMAYA <aamaya547@gmail.com>
2022-06-29 11:23:55 -0700

    copied over form from update page, set state

--- src/CreatePage.js
+++ src/CreatePage.js
@@ -3,7 +3,33 @@ import { useHistory } from 'react-router-dom';
 import { createTreasure } from './services/fetch-utils';
 
 export default function CreatePage() {
+  const { push } = useHistory();
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [source, setSource] = useState('');
+  const [rating, setRating] = useState('');
   return (
-    <div>CreatePage</div>
+    <div>
+      <h3>Create Treasure Specs</h3>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input onChange={(e) => setName(e.target.value)} value={name} />
+        </label>
+        <label>
+          Description:
+          <input onChange={(e) => setDescription(e.target.value)} value={description} />
+        </label>
+        <label>
+          Source:
+          <input onChange={(e) => setSource(e.target.value)} value={source} />
+        </label>
+        <label>
+          Rating:
+          <input onChange={(e) => setRating(e.target.value)} value={rating} type="number" />
+        </label>
+        <button>Create Treasure Spec</button>
+      </form>
+    </div>
   );
 }
